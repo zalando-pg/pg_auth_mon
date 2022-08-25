f91e7b63e418a2daea0f116071e4634a488b1522
Sergey Dudoladov <sergey.dudoladov@gmail.com>
2022-08-25 08:05:23 +0200

    sketch unit tests for logging

--- test.sh
+++ test.sh
@@ -8,6 +8,7 @@ export PGHOST=/tmp
 function cleanup() {
     pg_ctl -w stop -mf
     rm -fr $PGDATA $pwfile
+    sed --in-place "s#$PGHBA#PGHBA_PLACEHOLDER#g" expected/pg_auth_mon.out
 }
 
 cleanup 2> /dev/null
@@ -18,8 +19,23 @@ readonly pwfile=$(mktemp)
 echo -n $PGPASSWORD > $pwfile
 initdb --pwfile=$pwfile --auth=md5
 
-trap cleanup QUIT TERM EXIT
+# prepare tests of pg_auth_mon.log_successful_authentications
+cat >> test_cluster/postgresql.conf << EOF
+pg_auth_mon.log_successful_authentications = 'on'
+log_connections = 'off'
+log_destination = 'csvlog'
+log_directory = 'pg_log'
+log_filename = 'postgresql'
+logging_collector = 'on'
+EOF
 
+# the log line with a successful login info contains the full path to pg_hba.conf
+# so we need to have it in the expected output to pass the tests
+PGHBA=$(readlink -f test_cluster/pg_hba.conf)
+# use an alternative separator for sed, namely #, because $PGHBA is itself a path with slashes
+sed --in-place "s#PGHBA_PLACEHOLDER#$PGHBA#g" expected/pg_auth_mon.out
+
+trap cleanup QUIT TERM EXIT
 pg_ctl start -w -o "--shared_preload_libraries=pg_auth_mon --unix_socket_directories=$PGHOST"
 
 make USE_PGXS=1 installcheck || diff -u expected/pg_auth_mon.out results/pg_auth_mon.out
